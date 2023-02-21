50b6e9f7df91317ae55eb582ee4e55c3ea286497
Pedro Tonini Rosenberg Schneider <pedrotrschneider@gmail.com>
2023-02-21 14:08:25 -0300

    Seção 6 Aula 6

--- 06-parameters-and-exception-handling/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
+++ 06-parameters-and-exception-handling/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
@@ -22,6 +22,60 @@ public class MathController {
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
+    @GetMapping("/subtract/{numberOne}/{numberTwo}")
+    public Double subtract(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws UnsupportedMathOperationException {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please enter a numeric value.");
+        }
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    @GetMapping("/multiply/{numberOne}/{numberTwo}")
+    public Double multiply(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws UnsupportedMathOperationException {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please enter a numeric value.");
+        }
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    @GetMapping("/divide/{numberOne}/{numberTwo}")
+    public Double divide(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws UnsupportedMathOperationException {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please enter a numeric value.");
+        }
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    @GetMapping("/mean/{numberOne}/{numberTwo}")
+    public Double mean(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws UnsupportedMathOperationException {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please enter a numeric value.");
+        }
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+    }
+
+    @GetMapping("/sqrt/{number}")
+    public Double subtract(
+            @PathVariable(value = "number") String number
+    ) throws UnsupportedMathOperationException {
+        if (!isNumeric(number)) {
+            throw new UnsupportedMathOperationException("Please enter a numeric value.");
+        }
+        return Math.sqrt(convertToDouble(number));
+    }
+
     private boolean isNumeric(String strNumber) {
         if (strNumber == null) return false;
         strNumber = strNumber.replaceAll(",", ".");
