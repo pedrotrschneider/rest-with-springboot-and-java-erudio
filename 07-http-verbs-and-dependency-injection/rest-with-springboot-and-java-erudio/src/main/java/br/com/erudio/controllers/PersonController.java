ef54135cb51f5b67bf07ccae0c29f8f598394726
Pedro Tonini Rosenberg Schneider <pedrotrschneider@gmail.com>
2023-02-21 18:16:58 -0300

    Seção 7 Aula 3

--- 07-http-verbs-and-dependency-injection/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 07-http-verbs-and-dependency-injection/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -3,8 +3,9 @@ package br.com.erudio.controllers;
 import br.com.erudio.models.PersonModel;
 import br.com.erudio.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
@@ -13,8 +14,13 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public PersonModel findById(@PathVariable(value = "id") String id) throws Exception {
+    @GetMapping("/")
+    public List<PersonModel> findAll() {
+        return personService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonModel findById(@PathVariable(value = "id") String id) {
         return personService.findById(id);
     }
 }
--- 07-http-verbs-and-dependency-injection/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/services/PersonService.java
+++ 07-http-verbs-and-dependency-injection/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/services/PersonService.java
@@ -3,6 +3,8 @@ package br.com.erudio.services;
 import br.com.erudio.models.PersonModel;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,6 +13,16 @@ public class PersonService {
 
     static final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonService.class.getName());
+
+    public List<PersonModel> findAll() {
+        logger.info("Finding all people");
+        List<PersonModel> people = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            var personModel = mockPerson(i);
+            people.add(personModel);
+        }
+        return people;
+    }
 
     public PersonModel findById(String id) {
         logger.info("Finding one person by id.");
@@ -23,4 +35,13 @@ public class PersonService {
         return personModel;
     }
 
+    private PersonModel mockPerson(int i) {
+        var personModel = new PersonModel();
+        personModel.setId(counter.incrementAndGet());
+        personModel.setFirstName("First name " + i);
+        personModel.setLastName("Last Name " + i);
+        personModel.setAddress("Address " + i);
+        personModel.setGender("Gender " + i);
+        return personModel;
+    }
 }
