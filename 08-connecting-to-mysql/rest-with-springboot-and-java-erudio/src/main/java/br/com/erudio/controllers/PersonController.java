13fc501cd3dcdbcf2d5746f8c01c7d369316acf2
Pedro Tonini Rosenberg Schneider <pedrotrschneider@gmail.com>
2023-02-21 20:02:48 -0300

    Seção 8 Aula 6

--- 08-connecting-to-mysql/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 08-connecting-to-mysql/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -3,6 +3,9 @@ package br.com.erudio.controllers;
 import br.com.erudio.models.PersonModel;
 import br.com.erudio.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,28 +17,29 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<PersonModel> findAll() {
         return personService.findAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public PersonModel findById(@PathVariable(value = "id") Long id) {
         return personService.findById(id);
     }
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public PersonModel create(@RequestBody PersonModel personModel) {
         return personService.create(personModel);
     }
 
-    @PutMapping
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public PersonModel update(@RequestBody PersonModel personModel) {
         return personService.update(personModel);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable(value = "id") Long id) {
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         personService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
