8ff01564175d15291a70fea55fa0e6541f464103
Pedro Tonini Rosenberg Schneider <pedrotrschneider@gmail.com>
2023-02-21 19:16:48 -0300

    Seção 8 Aula 3

--- 08-connecting-to-mysql/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/models/PersonModel.java
+++ 08-connecting-to-mysql/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/models/PersonModel.java
@@ -1,16 +1,26 @@
 package br.com.erudio.models;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "person")
 public class PersonModel implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+    @Column(nullable = false)
     private String address;
+    @Column(nullable = false)
     private String gender;
 
     public PersonModel() {
