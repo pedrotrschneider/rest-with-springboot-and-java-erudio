bc689ccfee7d59de98001a2e6c45e8743d627225
Pedro Tonini Rosenberg Schneider <pedrotrschneider@gmail.com>
2023-02-27 10:40:03 -0300

    Seção 9 Aula 2

---
+++ 12-custom-serialization/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
@@ -0,0 +1,79 @@
+package br.com.erudio.data.vo.v1;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
+public class PersonVO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    @JsonProperty("first_name")
+    private String firstName;
+    @JsonProperty("last_name")
+    private String lastName;
+    private String address;
+    @JsonIgnore
+    private String gender;
+
+    public PersonVO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonVO that = (PersonVO) o;
+        return Objects.equals(id, that.id) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(address, that.address) && Objects.equals(gender, that.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, address, gender);
+    }
+}
