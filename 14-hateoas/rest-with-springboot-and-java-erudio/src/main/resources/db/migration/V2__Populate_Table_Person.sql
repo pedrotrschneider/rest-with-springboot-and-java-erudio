650c49d600d94c5da640d80318280c0649b74b79
Pedro Tonini Rosenberg Schneider <pedrotrschneider@gmail.com>
2023-02-27 12:10:45 -0300

    Seção 14 Aula 4

---
+++ 14-hateoas/rest-with-springboot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+    (3,'Anchiano - Italy','Leonardo','Male','daVinci'),
+    (6,'Anchiano - Italy','Ayrton','Male','daVinci'),
+    (8,'Porbandar - India','Indira','Female','Gandhi'),
+    (9,'Porbandar - India','Mahatma','Male','Gandhi'),
+    (11,'Kentucky - US','Muhammad','Male','Ali'),
+    (14,'Mvezo - South Africa','Nelson','Male','Mandela'),
+    (15,'Smiljan - Croatia','Nikola','Male','Tesla');
