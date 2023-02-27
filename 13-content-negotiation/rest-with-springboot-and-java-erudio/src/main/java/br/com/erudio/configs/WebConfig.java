01e3e013c6286bcaeaf1b8ba11611ee54c00a53c
Pedro Tonini Rosenberg Schneider <pedrotrschneider@gmail.com>
2023-02-27 11:07:25 -0300

    Seção 13 Aula 4

--- 13-content-negotiation/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/configs/WebConfig.java
+++ 13-content-negotiation/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/configs/WebConfig.java
@@ -10,9 +10,20 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        // Implementation via QUERY PARAM
+        /**
         configurer.favorParameter(true)
                 .parameterName("mediaType")
                 .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+         */
+
+        // Implementation via HEADER PARAM
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
