4a92e818ea47ea8505bcb0d8b72962e8c03305f1
Pedro Tonini Rosenberg Schneider <pedrotrschneider@gmail.com>
2023-03-01 12:00:54 -0300

    Seção 17 Aula 11

---
+++ 17-authentication-with-jwt/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/services/AuthService.java
@@ -0,0 +1,45 @@
+package br.com.erudio.services;
+
+import br.com.erudio.data.vo.v1.security.AccountCredentialsVO;
+import br.com.erudio.data.vo.v1.security.TokenVO;
+import br.com.erudio.repositories.UserRepository;
+import br.com.erudio.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public ResponseEntity signin(AccountCredentialsVO data) {
+        try {
+            var username = data.getUserName();
+            var password = data.getPassword();
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+            var user = userRepository.findByUsername(username);
+            if (user == null) {
+                throw new UsernameNotFoundException("Username " + username + " not found.");
+            }
+            
+            var tokenResponse = new TokenVO();
+            tokenResponse = tokenProvider.createAccessToken(username, user.getRoles());
+            return ResponseEntity.ok(tokenResponse);
+        } catch (Exception e) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+    }
+}
