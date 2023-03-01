181d6c5de4750a1f07977c2441293fe98de7df38
Pedro Tonini Rosenberg Schneider <pedrotrschneider@gmail.com>
2023-03-01 11:08:15 -0300

    Seção 17 Aula 8

--- 17-authentication-with-jwt/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/securityJwt/JwtTokenProvider.java
+++ 17-authentication-with-jwt/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/securityJwt/JwtTokenProvider.java
@@ -1,13 +1,22 @@
 package br.com.erudio.securityJwt;
 
 import br.com.erudio.data.vo.v1.security.TokenVO;
+import br.com.erudio.exceptions.InvalidJwtAuthenticationException;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.net.ssl.SSLPeerUnverifiedException;
 import java.util.Base64;
 import java.util.Date;
 import java.util.List;
@@ -41,9 +50,60 @@ public class JwtTokenProvider {
     }
 
     private String getAccessToken(String username, List<String> roles, Date now, Date validity) {
+        String issuerUrl = ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .build()
+                .toUriString();
+        return JWT.create()
+                .withClaim("roles", roles)
+                .withIssuedAt(now)
+                .withExpiresAt(validity)
+                .withSubject(username)
+                .withIssuer(issuerUrl)
+                .sign(algorithm)
+                .strip();
     }
 
     private String refreshToken(String username, List<String> roles, Date now) {
+        var validityRefreshToken = new Date(now.getTime() + (validityInMilliseconds * 3));
+        return JWT.create()
+                .withClaim("roles", roles)
+                .withIssuedAt(now)
+                .withExpiresAt(validityRefreshToken)
+                .withSubject(username)
+                .sign(algorithm)
+                .strip();
+    }
 
+    public Authentication getAuthentication(String token) {
+        var decodedJWT = decodeToken(token);
+        var userDetails = this.userDetailsService.loadUserByUsername(decodedJWT.getSubject());
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    private DecodedJWT decodeToken(String token) {
+        var tokenDecoderAlgorithm = Algorithm.HMAC256(secretKey.getBytes());
+        var verifier = JWT.require(tokenDecoderAlgorithm).build();
+        return verifier.verify(token);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        var bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring("Bearer ".length());
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        var decodedJWT = decodeToken(token);
+        try {
+            if (decodedJWT.getExpiresAt().before(new Date())) {
+                return false;
+            }
+            return true;
+        } catch (Exception e) {
+            throw new InvalidJwtAuthenticationException("Invalid JWT token.");
+        }
     }
 }
