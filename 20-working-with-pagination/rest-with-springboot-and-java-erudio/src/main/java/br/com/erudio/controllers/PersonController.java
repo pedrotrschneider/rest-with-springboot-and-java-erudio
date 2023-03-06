8aa1662bd2640bd0231f1c63d83588160af85e68
Pedro Tonini Rosenberg Schneider <pedrotrschneider@gmail.com>
2023-03-06 13:29:58 -0300

    Seção 20 Aula 4

--- 20-working-with-pagination/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 20-working-with-pagination/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -46,9 +47,11 @@ public class PersonController {
     )
     public ResponseEntity<Page<PersonVO>> findAll(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "limit", defaultValue = "12") Integer limit
+            @RequestParam(value = "size", defaultValue = "12") Integer size,
+            @RequestParam(value = "direction", defaultValue = "asc") String direction
     ) {
-        Pageable pageable = PageRequest.of(page, limit);
+        var sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
         return ResponseEntity.ok(personService.findAll(pageable));
     }
 
